78eb24747af7b13a2a3557eaf29e7cf72fba5ec5
quantumduck <quantum.duck@hotmail.com>
2018-07-06 18:56:18 -0400

    add single page app endpoints

--- sudokuApi.js
+++ sudokuApi.js
@@ -1,11 +1,26 @@
 var express = require('express');
+var path = require('path');
 var sudoku = require('./sudoku');
+
 var app = express();
 var fs = require("fs");
+var spaRoot = path.join(__dirname, 'spa');
 
 app.get('/sudoku/board', function (req, res) {
   res.set('Content-Type', 'application/json');
   res.end(JSON.stringify(sudoku.get()));
+});
+
+app.get('/', function (req, res) {
+  res.sendFile('index.html', {root: spaRoot});
+});
+
+app.get('/js/main.js', function (req, res) {
+  res.sendFile('js/main.js', {root: spaRoot});
+});
+
+app.get('/css/style.css', function (req, res) {
+  res.sendFile('css/style.css', {root: spaRoot});
 });
 
 var server = app.listen(8080, function () {
